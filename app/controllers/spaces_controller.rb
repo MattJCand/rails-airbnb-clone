c87041285e17f0448ffa3166aeeb8d50847bd4c1
Aurore Courtois <aurore.courtois@outlook.fr>
2017-05-22 16:26:15 +0200

    User can add space and see spaces

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -3,11 +3,27 @@ class SpacesController < ApplicationController
     @spaces = Space.all
   end
 
-  def new
+  def show
+    @space = Space.find(params[:id])
+  end
 
+  def new
+    @space = Space.new
   end
 
   def create
+    @space = Space.new(space_params)
+    @space.user = current_user
+    if @space.save
+      redirect_to space_path(@space)
+    else
+      render 'new'
+    end
+  end
 
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :address, :size, :price_by_day)
   end
 end
