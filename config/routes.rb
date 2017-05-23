ac46ddd22fdf37411dd786544c9d1eb76bc276dc
mathieu junes <matyjunes@gmail.com>
2017-05-23 17:04:52 +0200

    add controllers user

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   resources :spaces, only: [ :new, :index, :show, :create ]
+  resources :users, only: [:show]
   mount Attachinary::Engine => "/attachinary"
 end
