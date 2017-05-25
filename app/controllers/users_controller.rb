1a042830bcc98b8e29ebe9302435675504330ab6
Aurore Courtois <aurore.courtois@outlook.fr>
2017-05-25 12:24:15 +0200

    Added Venues and Booked Venues to User Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @bookings = @user.bookings
     @spaces = @user.spaces
+    @owner_bookings = Booking.where(space: @spaces)
   end
 end
