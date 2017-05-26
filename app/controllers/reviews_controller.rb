8b7555fe6abfbd04fe38e52be97500e5c1d2a0de
mathieu junes <matyjunes@gmail.com>
2017-05-26 11:10:08 +0200

    Review ok without Ajax

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   def create
     @space = Space.find(params[:space_id])
     @review = Review.new(review_params)
-    @space.restaurant = @space
+    @review.space = @space
     if @review.save
       redirect_to space_path(@space)
     else
